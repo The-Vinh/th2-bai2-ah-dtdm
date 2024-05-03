24e1538e4633459007964a54d6e5c36ee7eb2da4
The-Vinh <102274591+The-Vinh@users.noreply.github.com>
2024-05-03 10:20:39 +0700

    Update bai2.c

--- bai2.c
+++ bai2.c
@@ -1,10 +1,37 @@
-#include stdio.h
-int main(void){
-     Your code here!
-    for(int i=10; i=99; i++)
-    {
-        if(i % 7 ==0)
-            printf(%d ,i);
-    }
+#include <stdio.h>
+#include <math.h>
+
+int kiemtra_so_chinh_phuong(int n) {
+  if (sqrt(n) == (int)sqrt(n)) {
+    return 1;
+  } else {
     return 0;
+  }
+}
+
+int dem_so_chinh_phuong(int n) {
+  int dem = 0;
+  for (int i = 1; i <= n; i++) {
+    if (kiemtra_so_chinh_phuong(i)) {
+      dem++;
+    }
+  }
+  return dem;
+}
+
+int main() {
+  int n;
+  scanf("%d", &n);
+
+  int so_chinh_phuong = dem_so_chinh_phuong(n);
+  printf("So luong so chinh phuong nho hon %d: %d\n",n, so_chinh_phuong);
+
+  printf("Cac so chinh phuong nho hon %d la: ", n);
+  for (int i = n; i >=1; i--) {
+    if (kiemtra_so_chinh_phuong(i)==1 && i<n) {
+      printf("%d ", i);
+    }
+  }
+
+  return 0;
 }
